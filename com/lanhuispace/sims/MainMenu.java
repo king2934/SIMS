07eb8ff5c23cc0546f6020ef1c09e3e838821c85
king2934 <king2934@gmail.com>
2019-06-21 11:19:15 +0800

    更新

--- com/lanhuispace/sims/MainMenu.java
+++ com/lanhuispace/sims/MainMenu.java
@@ -2,6 +2,10 @@ package com.lanhuispace.sims;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends JMenuBar
 {
@@ -11,9 +15,24 @@ public class MainMenu extends JMenuBar
         JMenu editMenu = new JMenu("编辑");
         JMenu viewMenu = new JMenu("视图");
         JMenu aboutMenu = new JMenu("关于");
+		
+		JMenuItem newMenuItem = new JMenuItem("新建");
+		fileMenu.add(newMenuItem);		
+		
+		newMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("新建  被点击");
+            }
+        });
+		
 		this.add(fileMenu);
 		this.add(editMenu);
 		this.add(viewMenu);
 		this.add(aboutMenu);
 	}
+	
+	//
+	
+	
 }
