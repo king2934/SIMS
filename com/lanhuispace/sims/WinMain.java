711c1191a809502a008a48119fde98c2b7926c99
king2934 <king2934@gmail.com>
2019-06-25 17:28:58 +0800

    update

--- com/lanhuispace/sims/WinMain.java
+++ com/lanhuispace/sims/WinMain.java
@@ -3,6 +3,7 @@ package com.lanhuispace.sims;
 import javax.swing.JFrame;
 import java.awt.Color;
 import javax.swing.border.*;
+import java.awt.FlowLayout; 
 
 public class WinMain
 {
@@ -26,7 +27,7 @@ public class WinMain
 		mJFrame.setSize(1024,768);//w,h
 		mJFrame.setLocationRelativeTo(null);//居中
 		mJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mJFrame.setUndecorated(false);
+		mJFrame.setUndecorated(true);//默认的边框
 		//mJFrame.setBackground(Color.white);
 		mJFrame.getContentPane().setBackground(new Color(200,200,200,1));
 		
@@ -37,8 +38,10 @@ public class WinMain
 		Border margin = new EmptyBorder(1,0,1,0);
 		mm.setBorder(new CompoundBorder(border,margin));
 		
-		mJFrame.setJMenuBar(mm);		
-		mJFrame.setVisible(true);
+		mJFrame.setJMenuBar(mm);
+		mJFrame.setLayout(new FlowLayout()); 
+		mJFrame.getGraphicsConfiguration().getDevice().setFullScreenWindow(mJFrame);//setUndecorated=true	
+		mJFrame.setVisible(true);		
 	}
 }
 
